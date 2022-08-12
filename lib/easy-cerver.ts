15b9024a4ff883e01e5a99788c35f40c3fad8186
rajyan <rajyan@users.noreply.github.com>
2022-08-12 00:19:20 +0000

    Commit from GitHub Actions (Test and Fix)

--- lib/easy-cerver.ts
+++ lib/easy-cerver.ts
@@ -1,13 +1,8 @@
 import { Construct } from "constructs";
-import {
-  Stack,
-  StackProps,
-} from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 
 export class EasyCerver extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
-
   }
 }
