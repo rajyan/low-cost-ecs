bb7f057011279c6eef4503d8346945f0944f6af3
Yohta Kimura <kitakita7617@gmail.com>
2022-09-12 09:18:18 +0900

    chore: add examples to tsconfig dev

--- examples/scheduled-autoscaling.ts
+++ examples/scheduled-autoscaling.ts
@@ -1,32 +1,32 @@
-import { App } from "aws-cdk-lib";
+import { App } from 'aws-cdk-lib';
+import { Schedule } from 'aws-cdk-lib/aws-autoscaling';
 import { LowCostECS } from '../src';
-import {Schedule} from "aws-cdk-lib/aws-autoscaling";
 
 const app = new App();
 
-const stack = new LowCostECS(app, "LowCostECSStack", {
+const stack = new LowCostECS(app, 'LowCostECSStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
-  hostedZoneDomain: "rajyan.net",
-  recordDomainNames: ["test1.rajyan.net", "test2.rajyan.net"],
-  email: "kitakita7617@gmail.com",
-  hostInstanceSpotPrice: "0.0050",
+  hostedZoneDomain: 'rajyan.net',
+  recordDomainNames: ['test1.rajyan.net', 'test2.rajyan.net'],
+  email: 'kitakita7617@gmail.com',
+  hostInstanceSpotPrice: '0.0050',
 });
 stack.hostAutoScalingGroup.scaleOnSchedule('IncreaseAtMorning', {
   timeZone: 'Asia/Tokyo',
   schedule: Schedule.cron({
     minute: '0',
-    hour: '8'
+    hour: '8',
   }),
-  desiredCapacity: 1
-})
+  desiredCapacity: 1,
+});
 stack.hostAutoScalingGroup.scaleOnSchedule('DecreaseAtNight', {
   timeZone: 'Asia/Tokyo',
   schedule: Schedule.cron({
     minute: '0',
-    hour: '23'
+    hour: '23',
   }),
-  desiredCapacity: 0
-})
+  desiredCapacity: 0,
+});
