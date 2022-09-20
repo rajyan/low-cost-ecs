b2ac273662776e2a9f97fa9d560f4a576ac37665
Yohta Kimura <kitakita7617@gmail.com>
2022-09-21 05:05:57 +0900

    test: fix snapshot environment

--- test/snapshot.test.ts
+++ test/snapshot.test.ts
@@ -1,3 +1,6 @@
+process.env.CDK_DEFAULT_ACCOUNT = 'test-account';
+process.env.CDK_DEFAULT_REGION = 'test-region';
+
 import { Template } from 'aws-cdk-lib/assertions';
 import { allPropsStack } from '../examples/all-props';
 import { autoscalingStack } from '../examples/autoscaling';
@@ -8,7 +11,7 @@ expect.addSnapshotSerializer({
   serialize: (val) => {
     return `"${val.replace(
       new RegExp(
-        `[a-f0-9]{64}\\.zip|cdk-[a-z0-9]{9}-assets-\\$\{AWS::AccountId}-\\$\{AWS::Region}|container-assets-${minimumStack.account}-${minimumStack.region}:[a-z0-9]{64}`,
+        '[a-f0-9]{64}\\.zip|cdk-[a-z0-9]{9}-assets-\\$\{AWS::AccountId}-\\$\{AWS::Region}|container-assets-test-account-test-region:[a-z0-9]{64}',
       ),
       '[HASH REMOVED]',
     )}"`;
